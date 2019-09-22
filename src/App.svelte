e0e64e93a5ddd1c3bb7d5448ea5eaf41443d3dd8
frencojobs <frenco.willian@gmail.com>
2019-09-22 20:47:17 +0630

    updated content

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,69 @@
 <script>
-  let name = "Frenco"
+  /**
+   * OK so lets talk about the fact that
+   * I used slevte to create a normal static content website
+   * yeah, I get it but
+   * I am bored so I do everything I want
+   * if it annoyed you so
+   * you're welcome.
+   * */
 </script>
 
-<h1>{name}</h1>
+<style>
+  main {
+    font-family: "Source Code Pro", monospace;
+    width: 70%;
+    margin: auto;
+  }
+  @media screen and (max-width: 600px) {
+    main {
+      width: 90%;
+    }
+  }
+  article,
+  footer {
+    line-height: 1.5em;
+    margin: 2em 0;
+  }
+  footer > span,
+  a {
+    cursor: pointer;
+    text-decoration: underline;
+    color: blue;
+  }
+</style>
+
+<main>
+  <h1>Frenco Jobs</h1>
+  <article>
+    I am currently attending classes at University of Information Technology,
+    Myanmar.
+    <br />
+    And I have a little bit of web development knowledge so I built this site.
+    <br />
+    I can type about 50 words per minute, and I like to listen LAUV songs on the
+    bus.
+    <br />
+    Elon Musk is my role model.
+    <br />
+    I like cats and I usually pretend to love big dogs even though I am so much
+    afraid of them.
+    <br />
+    I hate to use chat abbreviations like LOL, BTW, & I don't like emojis
+    either.
+    <br />
+    BTW, I've seen two TESLA in real life, and I ❤️ them a lot.
+  </article>
+  <footer>
+    <a href="https://blog.frenco.codes">[My Blog]</a>
+    &middot;
+    <a href="https://github.com/frencojobs">[Github]</a>
+    &middot;
+    <a href="https://twitter.com/frencojobs">[Twitter]</a>
+    &middot;
+    <span>[Broken]</span>
+  </footer>
+  <span title="for idiots: PS stands for Post Scriptum">
+    P.S. the last link is really broken
+  </span>
+</main>
