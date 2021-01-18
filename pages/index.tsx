4f40b16ff8d8e5494134fe553e7fdc72c0fda1e6
Frenco <hey@frenco.dev>
2021-01-18 17:10:48 +0630

    feat: start fresh

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen"></div>
+  )
+}
+
+export default IndexPage
