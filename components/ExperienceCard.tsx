b4c7cfad29749b3f12390c4e7da8a31f3b6f2593
Frenco <hey@frenco.dev>
2021-01-19 11:50:25 +0630

    feat: add links to cards

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -7,33 +7,35 @@ type Props = {
 export const ExperienceCard: React.FC<Props> = ({ item }) => {
   return (
     <li className="transition transform border-2 border-gray-300 border-solid rounded-md dark:border-gray-500 hover:-translate-y-1 hover:shadow-md">
-      {item.cover ? (
-        <>
-          <div className="relative w-full h-60 md:h-52 lg:h-40">
-            <Image
-              src={item.cover}
-              alt={`Cover for ${item.name}`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-t-md"
-            />
-          </div>
-          <div className="w-full border-t border-gray-300 border-solid dark:border-gray-500" />
-        </>
-      ) : null}
-      <div className="px-4 py-2">
-        <h1 className="text-lg font-bold text-gray-800 dark:text-white">
-          {item.name}
-        </h1>
-        <span className="text-gray-600 dark:text-gray-200">
-          {item.position}
-        </span>
-        <div className="h-3" />
-        <span className="text-sm text-gray-600 dark:text-gray-200">
-          {item.duration}
-        </span>
-        <div className="h-1" />
-      </div>
+      <a href={item.url} rel="noreferrer" target="_blank">
+        {item.cover ? (
+          <>
+            <div className="relative w-full h-60 md:h-52 lg:h-40">
+              <Image
+                src={item.cover}
+                alt={`Cover for ${item.name}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-md"
+              />
+            </div>
+            <div className="w-full border-t border-gray-300 border-solid dark:border-gray-500" />
+          </>
+        ) : null}
+        <div className="px-4 py-2">
+          <h1 className="text-lg font-bold text-gray-800 dark:text-white">
+            {item.name}
+          </h1>
+          <span className="text-gray-600 dark:text-gray-200">
+            {item.position}
+          </span>
+          <div className="h-3" />
+          <span className="text-sm text-gray-600 dark:text-gray-200">
+            {item.duration}
+          </span>
+          <div className="h-1" />
+        </div>
+      </a>
     </li>
   )
 }
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -24,54 +24,56 @@ export const ProjectCard: React.FC<Props> = ({
           block: !useFlex,
         }
       )}>
-      {item.cover ? (
-        <>
-          <div className="relative w-full h-60 md:h-52 lg:h-40">
-            <Image
-              src={item.cover}
-              alt={`Cover for ${item.name}`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-t-md"
-            />
-          </div>
-          <div className="w-full border-t border-gray-300 border-solid dark:border-gray-500" />
-        </>
-      ) : null}
-      <div className="flex flex-col justify-between w-full px-4 py-2">
-        <div>
-          <h1 className="text-lg font-bold text-gray-800 dark:text-white">
-            {item.name}
-          </h1>
-          <span className="text-gray-600 dark:text-gray-200">
-            {item.tagline}
-          </span>
-        </div>
-        <div className="h-3" />
-        <div className="flex flex-row items-center justify-between">
-          <div className="flex flex-row items-center">
-            {item.stack.map((name) => (
-              <img
-                src={`/icons/${name.toLocaleLowerCase()}.svg`}
-                key={name}
-                className="w-4 h-4 mr-3"
+      <a href={item.url} rel="noreferrer" target="_blank">
+        {item.cover ? (
+          <>
+            <div className="relative w-full h-60 md:h-52 lg:h-40">
+              <Image
+                src={item.cover}
+                alt={`Cover for ${item.name}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-t-md"
               />
-            ))}
-          </div>
-          {star ? (
-            <span className="flex flex-row items-center text-sm">
-              <img
-                src={`/icons/github-star-${
-                  resolvedTheme === 'dark' ? 'light' : 'dark'
-                }.svg`}
-                className="w-3 mr-1"
-              />
-              {star}
+            </div>
+            <div className="w-full border-t border-gray-300 border-solid dark:border-gray-500" />
+          </>
+        ) : null}
+        <div className="flex flex-col justify-between w-full px-4 py-2">
+          <div>
+            <h1 className="text-lg font-bold text-gray-800 dark:text-white">
+              {item.name}
+            </h1>
+            <span className="text-gray-600 dark:text-gray-200">
+              {item.tagline}
             </span>
-          ) : null}
+          </div>
+          <div className="h-3" />
+          <div className="flex flex-row items-center justify-between">
+            <div className="flex flex-row items-center">
+              {item.stack.map((name) => (
+                <img
+                  src={`/icons/${name.toLocaleLowerCase()}.svg`}
+                  key={name}
+                  className="w-4 h-4 mr-3"
+                />
+              ))}
+            </div>
+            {star ? (
+              <span className="flex flex-row items-center text-sm">
+                <img
+                  src={`/icons/github-star-${
+                    resolvedTheme === 'dark' ? 'light' : 'dark'
+                  }.svg`}
+                  className="w-3 mr-1"
+                />
+                {star}
+              </span>
+            ) : null}
+          </div>
+          <div className="h-1" />
         </div>
-        <div className="h-1" />
-      </div>
+      </a>
     </li>
   )
 }
